fb7d661221ea4d36e3fc30a6e4db4ab9256e2a9e
Akiri017 <mariannesantos174@gmail.com>
2026-03-29 21:56:24 +0800

    Fix Vercel deployment: ignore node_modules, add vercel.json

--- dist/diagnostics.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DiagnosticSeverity = void 0;
-var DiagnosticSeverity;
-(function (DiagnosticSeverity) {
-    DiagnosticSeverity["Error"] = "error";
-    DiagnosticSeverity["Warning"] = "warning";
-})(DiagnosticSeverity || (exports.DiagnosticSeverity = DiagnosticSeverity = {}));
-//# sourceMappingURL=diagnostics.js.map
--- dist/language.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const LANGUAGE_NAME = "Low Cortisol Programming Language";
-export declare const DATA_TYPES: readonly ["decimal", "doubleDecimal", "letters", "letter", "int", "bool"];
-export type DataTypeLexeme = (typeof DATA_TYPES)[number];
-export declare const OUTPUT_KEYWORDS: readonly ["display", "displayln", "displayf"];
-export type OutputKeywordLexeme = (typeof OUTPUT_KEYWORDS)[number];
-export declare const ASSIGN_OPERATOR: ":";
-export declare const STATEMENT_DELIMITER: ".";
-//# sourceMappingURL=language.d.ts.map
